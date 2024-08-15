8f45ef18228a9bd1ea4838299a4d9ddbb2508dc0
adithya <68180393+adithya0727@users.noreply.github.com>
2024-08-15 17:31:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,88 +1,67 @@
-const express = require('express');
+const express = require('express')
 require('dotenv').config();
-const app = express();
+const app = express()
 
 const bodyparser = require('body-parser');
 const https = require("https");
 
-app.use(bodyparser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(bodyparser.urlencoded({ extended: true }))
+app.use(express.static("public"))
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/index.html")
+})
+app.post("/", function(req,res){
+    var firstname = req.body.fname
+    var lastname = req.body.lname
+    var email = req.body.email
+    console.log(firstname,lastname,email)
 
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/index.html");
-});
 
-app.post("/", function(req, res) {
-    const firstname = req.body.fname;
-    const lastname = req.body.lname;
-    const email = req.body.email;
-    console.log(firstname, lastname, email);
 
-    // Data to be sent in the POST request body
-    const data = {
-        key: process.env.API_KEY,  // Mandrill API key from environment variables
-        message: {
-            from_email: "your_email@example.com",
-            to: [
-                {
-                    email: email,
-                    name: firstname + " " + lastname,
-                    type: "to"
-                }
-            ],
-            subject: "Welcome!",
-            text: "Hello " + firstname + ", welcome to our service!"
+
+
+var data = {
+  members : [
+    {
+        email_address : email,
+        status: "subscribed",
+        merge_fields : {
+            FNAME : firstname,
+            LNAME : lastname
         }
-    };
+    }
+  ]  
+};
+const jsondata = JSON.stringify(data);
+const api_key = process.env.API_KEY;
+const url = "https://us21.api.mailchimp.com/3.0/lists/acf856d8bf/members"
 
-    const jsondata = JSON.stringify(data);
-    const url = "https://mandrillapp.com/api/1.0/messages/send.json";
-
-    const options = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        }
-    };
-
-    const request = https.request(url, options, function(response) {
-        let responseData = '';
-        response.on('data', function(chunk) {
-            responseData += chunk;
-        });
-
-        response.on('end', function() {
-            const parsedData = JSON.parse(responseData);
-            console.log(parsedData);
-
-            if (response.statusCode === 200) {
-                res.sendFile(__dirname + "/success.html");
-            } else {
-                res.sendFile(__dirname + "/failure.html");
-            }
-        });
-    });
-
-    request.on('error', function(e) {
-        console.error(e);
-        res.sendFile(__dirname + "/failure.html");
-    });
-
-    request.write(jsondata);
-    request.end();
-});
-
-app.post("/failure", function(req, res) {
-    res.redirect("/");
-});
-
-app.listen(process.env.PORT || 3000, function() {
-    console.log('listening on port 3000');
-});
+const options = {
+    method: "POST",
+        "Key": + api_key
+};
 
 
-
-app.listen(process.env.PORT || 3000, function(){
-    console.log('listening on port 3000')
+const request = https.request(url,options,function(response){
+    
+    if(response.statusCode === 200){
+        res.sendFile(__dirname + "/success.html")
+    }
+    else{
+        res.sendFile(__dirname + "/failure.html")
+    }
+    
+    
+    
+    
+    response.on("data",function(data){
+        console.log(JSON.parse(data));
+    })
+})
+ request.write(jsondata)
+ request.end();
 })
 
+app.post("/failure",function(req,res){
+    res.redirect("/")
+})
