8443b84e8c542b3061f5ea205894b080b6bbd5c2
adithya <68180393+adithya0727@users.noreply.github.com>
2024-08-16 23:04:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ const request = https.request(url,options,function(response){
 app.post("/failure",function(req,res){
     res.redirect("/")
 })
-
-app.listen(3000, function() {
-    console.log("Server is running on port 3000");
-  });
+const port = process.env.PORT || 3000;
+app.listen(port, '0.0.0.0', function() {
+    console.log("Server is running on port " + port);
+}); 
